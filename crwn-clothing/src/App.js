d837073925aa82cc346c69bcff0fd1af5a087862
sumitrajneesh <rajneeshsumit111.sk@gmail.com>
2020-06-21 00:56:03 +0530

    commit

--- crwn-clothing/src/App.js
+++ crwn-clothing/src/App.js
@@ -1,11 +1,20 @@
 import React from 'react';
 import './App.css';
 import HomePage from './pages/homepages/homepage.component'
+import{Switch,Route} from 'react-router-dom';
+import ShopPage from './pages/shop/shop.component';
+import Header from './components/header/header.component';
+import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 
 function App() {
   return (
     <div>
-     <HomePage/>
+      <Header/>
+      <Switch>
+     <Route exact path='/' component={HomePage}/>
+     <Route  path='/shop' component={ShopPage}/>
+     <Route path='/signin' component={SignInAndSignUpPage}/>
+     </Switch>
     </div>
   );
 }
