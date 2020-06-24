ceedb39b1c303b9cbcde10100dfb097aaa3fb3f2
sumitrajneesh <rajneeshsumit111.sk@gmail.com>
2020-06-24 14:35:54 +0530

    commit

--- crwn-clothing/src/App.js
+++ crwn-clothing/src/App.js
@@ -1,41 +1,38 @@
 import React from 'react';
 import './App.css';
 import HomePage from './pages/homepages/homepage.component'
-import{Switch,Route} from 'react-router-dom';
+import{Switch,Route,Redirect} from 'react-router-dom';
 import ShopPage from './pages/shop/shop.component';
 import Header from './components/header/header.component';
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
+import {connect} from 'react-redux';
+import {setCurrentUser} from './redux/user/user.actions';
 
 import {auth,createUserProfileDocument} from './firebase/firebase.utils';
 
 class App extends React.Component {
 
-  constructor(props){
-    super(props);
-
-    this.state = {
-      currentUser: null
-    }
-  }
-
   unsubscribeFromAuth = null
 
   componentDidMount(){
+
+    const {setCurrentUser} = this.props
     this.unsubscribeFromAuth = auth.onAuthStateChanged( async userAuth => {
       if(userAuth){
         const userRef = await createUserProfileDocument(userAuth);
         userRef.onSnapshot(snapShot => {
-         this.setState({
+         setCurrentUser({
            currentUser:{
              id:snapShot.id,
              ...snapShot.data()
            }
          });
 
-         console.log(this.state);
-
         })
       }
+
+      setCurrentUser(userAuth)
+
       
      
     } )
@@ -46,13 +43,14 @@ class App extends React.Component {
   }
 
   render(){
+    const {currentUser} = this.props
     return (
       <div>
-        <Header currentUser={this.state.currentUser}/>
+        <Header />
         <Switch>
        <Route exact path='/' component={HomePage}/>
        <Route  path='/shop' component={ShopPage}/>
-       <Route path='/signin' component={SignInAndSignUpPage}/>
+       <Route exact path='/signin' render={() => currentUser ? (<Redirect to='/' />): (<SignInAndSignUpPage/>)}/>
        </Switch>
       </div>
     );
@@ -61,4 +59,12 @@ class App extends React.Component {
   
 }
 
-export default App;
+const mapStateToProps = ({user}) => ({
+  currentUser:user.currentUser
+})
+
+const mapDispatchToProps = dispatch => ({
+  setCurrentUser: user => dispatch(setCurrentUser(user))
+})
+
+export default connect(mapStateToProps,mapDispatchToProps) (App);
--- crwn-clothing/src/components/header/header.component.jsx
+++ crwn-clothing/src/components/header/header.component.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
+import {connect} from 'react-redux';
 
 import {ReactComponent as Logo} from '../../assets/crown.svg';
 import {auth} from '../../firebase/firebase.utils';
+import CartIcon from '../cart-icon/cart-icon.compoent';
+import CartDropdown from '../cart-dropdown/cart-dropdown.component';
 
 import './header.styles.scss';
 
-const Header = ({currentUser}) => (
+
+const Header = ({currentUser ,hidden}) => (
     <div className="header">
         <Link className='logo-container' to='/'>
             <Logo className="logo"/>
@@ -27,8 +31,20 @@ const Header = ({currentUser}) => (
                     SIGN IN
                 </Link>
             }
+
+            <CartIcon/>
         </div>
+        {
+            hidden?null:
+            <CartDropdown/>
+        }
+        
     </div>
 )
 
-export default Header;
+const mapStateToProps = ({user:{currentUser},cart:{hidden}}) => ({
+    currentUser,
+    hidden
+})
+
+export default connect(mapStateToProps) (Header);
