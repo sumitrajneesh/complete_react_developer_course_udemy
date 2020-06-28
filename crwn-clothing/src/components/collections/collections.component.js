b5451a16194cd1819ecf1a2be597e75426c87b4d
sumitrajneesh <rajneeshsumit111.sk@gmail.com>
2020-06-28 15:59:11 +0530

    commit

--- crwn-clothing/src/components/collections/collections.component.js
+++ crwn-clothing/src/components/collections/collections.component.js
@@ -3,12 +3,17 @@ import React from 'react';
 import {connect} from 'react-redux';
 import './collections.styles.scss';
 import {selectCollection} from '../../redux/shop/shop.selectors';
+import CollectionItem from '../collection-item/collection-item.component';
+
 
 const CollectionPage = ({collection}) => {
-    console.log(collection)
+   const {title,items} = collection;
     return(
         <div className="collection-page">
-            <h2>Collection Page</h2>
+            <h2 className="title">{title}</h2>
+            <div className="items">
+                {items.map((item) => (<CollectionItem key={item.id} item={item}/>))}
+            </div>
         </div>
     )
 }
