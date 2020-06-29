02a2153c07ca2e945740a31738ee89c83f9debec
sumitrajneesh <rajneeshsumit111.sk@gmail.com>
2020-06-29 17:01:49 +0530

    commit

--- crwn-clothing/src/components/custom-button/custom-button.component.jsx
+++ crwn-clothing/src/components/custom-button/custom-button.component.jsx
@@ -1,12 +1,10 @@
 import React from 'react';
 
 
-import './custom-button.styles.scss';
+import {CustomerButtonContainer} from './custom-button.styles';
 
-const CustomButton = ({children,isGoogleSignIn,inverted,...otherProps}) => (
-    <button className={ `${inverted ? 'inverted':''}${isGoogleSignIn ? 'google-sign-in':''} custom-button`} {...otherProps}>
-        {children}
-    </button>
+const CustomButton = ({children,...props}) => (
+<CustomerButtonContainer {...props} >{children}</CustomerButtonContainer>
 )
 
 export default CustomButton;
