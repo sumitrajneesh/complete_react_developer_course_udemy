6a35a5e88ef018b665a04765d989de21cca84d5b
sumitrajneesh <rajneeshsumit111.sk@gmail.com>
2020-06-29 11:19:22 +0530

    intrigation with payment

--- crwn-clothing/src/pages/checkout/checkout.component.jsx
+++ crwn-clothing/src/pages/checkout/checkout.component.jsx
@@ -3,6 +3,7 @@ import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
 
 import CheckoutItem from '../../components/checkout-item/checkout-item.component';
+import StripeCheckoutButton from '../../components/stripe-button/stripe-button.component';
 
 import {
   selectCartItems,
@@ -10,6 +11,7 @@ import {
 } from '../../redux/cart/cart.selectors';
 
 import './checkout.styles.scss';
+
 
 const CheckoutPage = ({ cartItems, total }) => (
   <div className='checkout-page'>
@@ -34,6 +36,12 @@ const CheckoutPage = ({ cartItems, total }) => (
       <CheckoutItem key={cartItem.id} cartItem={cartItem} />
     ))}
     <div className='total'>TOTAL: ${total}</div>
+    <div className="test-warning">
+        *Please use the following test credit card* 
+        <br/>
+        4242 4242 4242 4242 -Exp: 10/20 -CVV:123
+    </div>
+    <StripeCheckoutButton price={total} />
   </div>
 );
 
